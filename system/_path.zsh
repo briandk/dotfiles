15f93179b23505045c887292355cad82d94f99e9
Brian Danielak <brian@briandk.com>
2015-08-02 22:08:40 -0400

    Add configs for anaconda python

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,6 @@
 # TeXLive
 TEXLIVE=/usr/local/texlive/2015/bin/x86_64-darwin
+ANACONDA=$HOME/anaconda/bin
 
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TEXLIVE:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TEXLIVE:$ANACONDA:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
