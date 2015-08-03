0b1a1d0b99a46f133310db71d1909bd7e80a6353
Brian Danielak <brian@briandk.com>
2015-08-02 21:41:20 -0400

    Migrate aliases and PATH settings from my .profile

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+# TeXLive
+TEXLIVE=/usr/local/texlive/2015/bin/x86_64-darwin
+
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TEXLIVE:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
