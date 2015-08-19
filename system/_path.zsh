49682ab3244bb3ae3a277b93f3cd0c2a87e21e99
Brian Danielak <brian@briandk.com>
2015-08-18 22:58:05 -0400

    Add Haskell's cabal to PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,5 +2,5 @@
 TEXLIVE=/usr/local/texlive/2015/bin/x86_64-darwin
 ANACONDA=$HOME/anaconda/bin
 
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TEXLIVE:$ANACONDA:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TEXLIVE:$ANACONDA:~/.cabal/bin:~/Library/Haskell/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
