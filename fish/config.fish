ef8af83f074dc3e0bac714c60f4633aadacca7bc
Brian Danielak <briandaniela.k+github@gmail.com>
2018-09-01 11:42:28 -0400

    Update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,32 @@
 
 # set --export NVM_DIR export NVM_DIR="$HOME/.nvm"
 #   . "/usr/local/opt/nvm/nvm.sh"
+
+# Git aliases
+alias gss="git status -sb"
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+
+# Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+
+alias gai='git add --interactive'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gcb='git copy-branch-name'
+alias gb='git branch'
+alias gac='git add -A & git commit -m'
+alias ge='git-edit-new'
+alias go='git checkout'
+
+# Anaconda Python
+set -gx PATH /anaconda3/bin $PATH
+source /anaconda3/etc/fish/conf.d/conda.fish
+
+# Source secure variables
+source ~/dev/dotfiles/secure/secure-environment-variables.fish
+
+# RVM Ruby Version Manager
+/bin/bash /etc/profile
